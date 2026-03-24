4908ad03754a45a2c9ea2619bcf14c48262108ff
Adrian Acarreta <alu.adrian.acarreta@mataro.epiaedu.cat>
2026-03-24 16:02:48 +0100

    Connexió amb el javascript

---
+++ js/config.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyC7w6IXhgMd6pssTKcxxrWBZS404Abo0oU",
+    authDomain: "items-85117.firebaseapp.com",
+    projectId: "items-85117",
+    storageBucket: "items-85117.firebasestorage.app",
+    messagingSenderId: "617013868224",
+    appId: "1:617013868224:web:2fb8584f4e3c3fbab31f25"
+});
+
+const auth = firebase.auth();
